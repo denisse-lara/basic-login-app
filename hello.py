afb9eb3245a5e3f78236bf7302738570e13229c6
Denisse Lara <denisse.lara@goluuk.com>
2022-03-07 13:40:12 -0400

    add a login route with its template

--- hello.py
+++ hello.py
@@ -1,16 +1,36 @@
 import os
 
-from flask import Flask
+from flask import Flask, render_template, url_for, redirect, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Wow"
+    # import pdb; pdb.set_trace()
+    return '<a href="/login">Login</a>'
 
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if valid_login(request.form['username'], request.form['password']):
+            return redirect(url_for('home', username=request.form['username']))
+        else:
+            error = 'Invalid username and password combination'
+    return render_template('login.html', error=error)
+
+@app.route('/home/<username>')
+def home(username=None):
+    return render_template('home.html', username=username)
+
+def valid_login(username, password):
+    if password == "1234":
+        return True
+    
+    return False
 
 if __name__ == '__main__':
     host = os.getenv('IP', '0.0.0.0')
     port = int(os.getenv('PORT', 5000))
-
+    app.debug = True
     app.run(host=host, port=port)
