e782d9934b6dfa726fecfef863e7b08bce6966db
Denisse Lara <denisse.lara@goluuk.com>
2022-03-07 14:11:30 -0400

    add flash messages and a base.html

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 import os
 
-from flask import Flask, render_template, url_for, redirect, request
+from flask import Flask, render_template, url_for, redirect, request, flash
 
 app = Flask(__name__)
 
@@ -14,7 +14,8 @@ def login():
     error = None
     if request.method == 'POST':
         if valid_login(request.form['username'], request.form['password']):
-            return redirect(url_for('home', username=request.form['username']))
+            flash("Successfully logged in")
+            return redirect(url_for('home', username=request.form.get('username')))
         else:
             error = 'Invalid username and password combination'
     return render_template('login.html', error=error)
@@ -33,4 +34,5 @@ if __name__ == '__main__':
     host = os.getenv('IP', '0.0.0.0')
     port = int(os.getenv('PORT', 5000))
     app.debug = True
+    app.secret_key = os.getenv('KEY')
     app.run(host=host, port=port)
