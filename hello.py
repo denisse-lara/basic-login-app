9c6c30f16a35ed94f0cefa612b1aab5eeb768bf7
Denisse Lara <denisse.lara@goluuk.com>
2022-03-09 08:59:40 -0400

    fix(secretkey): change secret key env var name

--- hello.py
+++ hello.py
@@ -76,7 +76,7 @@ if __name__ == "__main__":
     host = os.getenv("IP", "0.0.0.0")
     port = int(os.getenv("PORT", 5000))
     app.debug = True
-    app.secret_key = os.getenv("KEY")
+    app.secret_key = os.getenv("SECRET_KEY")
 
     # logging
     handler = RotatingFileHandler("error.log", maxBytes=10000, backupCount=1)
